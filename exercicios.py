245c2452b0fffe6b8052229d5d7f8bca6a352561
Alessandro Bernardino <ale.santos953@gmail.com>
2025-08-17 14:25:21 -0300

    usando o isinstance

--- exercicios.py
+++ exercicios.py
@@ -7,6 +7,13 @@ resultado = numero_01 // numero_02
 print(resultado) """
 
 #programa para calcular a área de um circulo
-raio_circulo = float(input("Insira o raio: "))
+""" raio_circulo = float(input("Insira o raio: "))
 area_circulo =  math.pi * raio_circulo**2
-print(f"a área é: {area_circulo:.2f}")
+print(f"a área é: {area_circulo:.2f}") """
+
+#verificando se um número é um inteiro
+numero = input("Insira um número: ")
+if isinstance(numero,int):
+  print("o número é inteiro")
+else:
+  print('o número não é inteiro')
