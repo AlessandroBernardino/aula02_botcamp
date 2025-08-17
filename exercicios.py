d989975ec962ce1035ec9e4532f14b8e55b497a5
Alessandro Bernardino <ale.santos953@gmail.com>
2025-08-17 14:07:13 -0300

    adicionado comentário

--- exercicios.py
+++ exercicios.py
@@ -6,6 +6,7 @@ numero_02 = int(input('Insira um numero inteiro: '))
 resultado = numero_01 // numero_02
 print(resultado) """
 
+#programa para calcular a área de um circulo
 raio_circulo = float(input("Insira o raio: "))
 area_circulo =  math.pi * raio_circulo**2
 print(f"a área é: {area_circulo:.2f}")
