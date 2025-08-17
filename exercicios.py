274172764bae108b8ad3f6e151d1a376e3addb95
Alessandro Bernardino <ale.santos953@gmail.com>
2025-08-17 14:05:59 -0300

    adicionado exercicio 10

--- exercicios.py
+++ exercicios.py
@@ -1,6 +1,11 @@
 #programa que pede dois números inteiros e imprime a divisão inteira do primeiro pelo segundo
+import math
 
-numero_01 = int(input('Insira um numero inteiro: '))
+""" numero_01 = int(input('Insira um numero inteiro: '))
 numero_02 = int(input('Insira um numero inteiro: '))
 resultado = numero_01 // numero_02
-print(resultado)
+print(resultado) """
+
+raio_circulo = float(input("Insira o raio: "))
+area_circulo =  math.pi * raio_circulo**2
+print(f"a área é: {area_circulo:.2f}")
